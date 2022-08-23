13b9dea35f8c5a64ca37ceaa20003a00e2b7d145
joshuamkite <joshuamkite@gmail.com>
2022-08-23 09:04:05 +0100

    rationalise local name

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  s3_origin_id   = "${var.domain_name}-cloudfront-orgin-id"
+  s3_origin_id   = "${var.domain_name}-orgin-id"
   s3_bucket_name = var.s3_bucket_custom_name == "" ? var.domain_name : var.s3_bucket_custom_name
 }
